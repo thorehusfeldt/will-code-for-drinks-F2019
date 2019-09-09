d38630f88273fc1bc76b630525bd2b020765a406
Thore Husfeldt <thore.husfeldt@gmail.com>
2019-09-09 19:21:49 +0200

    Cleaner AC solution

--- weightedintervalscheduling/submissions/accepted/bottom-up.py
+++ weightedintervalscheduling/submissions/accepted/bottom-up.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 import bisect
+from sys import stdin
 n = int(input())
-I = [tuple(map (int, input().split())) for _ in range(n)]
-for s, f, v in I:
-    assert (0 <= s < f <= 10**9)
-    assert (1 <= v <= 10**9)
-I.sort(key = lambda t: t[1]) # sort by finish time, earliest first
+I = []
+for line in stdin:
+    s, f, w = map(int, line.split())
+    I.append((f,s,w)) # finish time first, to make sorting easier
 
-
-p = [None] * n # p[j] = highest index of interval still fitting before I[j] (None otherwise)
-F = [t[1] for t in I]
-for i in range(n):
-    s = I[i][0]
-    p[i] = bisect.bisect_right(F, s) - 1
-
+I.sort() 
+F = [f for f, _, _ in I] # finish times (for the binary search below)
 M = [0]
-for j in range(1, n + 1):
-    M.append(max(I[j-1][2] + (M[p[j-1] +1] if p[j-1] is not None else 0), M[j-1]))
+for _, s, w in I:
+    prev = bisect.bisect_right(F, s)
+    take = w + (M[prev] if prev else 0)
+    drop = M[-1]
+    M.append( max(take, drop) )
 
-assert (1 <= M[n] <= 10**9)
-print (M[n])
+assert (1 <= M[-1] <= 10**9)
+print (M[-1])
