df7fd1f46c855fc73a3e88b75825f14183f6a631
Thore Husfeldt <thore.husfeldt@gmail.com>
2019-09-19 17:28:00 +0200

    More data for elderscrollbar

---
+++ elderscrollbar/input_format_validators/validate.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+from sys import stdin
+import sys
+import re
+
+integer = "(0|[1-9]\d*)"
+
+line = stdin.readline()
+assert re.match(integer + ' ' + integer + ' '  + integer + ' ' + integer +'\n', line), 'first line: expected four integers'
+W, H, L, N = map(int, line.split())
+assert 3 <= W <= 200
+assert 3 <= H <= 200
+assert 1 <= N <= 10**5
+
+textlines = []
+for _ in range(N):
+    line = stdin.readline()
+    assert re.match('\w.*\w\n', line)
+    assert len(line) <= 81
+    textlines.append(line.strip())
+text = ' '.join(textlines)
+words = text.split()
+
+i = 0
+typesettext = list()
+line = ""
+while i < len(words):
+    next_word = words[i]
+    if len(line) == 0 and len(next_word) >= W:
+        line += next_word[:W]
+        i += 1
+        continue
+    if len(line) + len(next_word) + 1 <= W:
+        line += (" " if len(line) > 0 else "") + next_word 
+        i += 1
+        continue
+    else:
+        typesettext.append(line)
+        line = ""
+if (line):
+        typesettext.append(line)
+
+assert L + H <= len(typesettext)
+
+assert not stdin.readline()
+# Nothing to report
+sys.exit(42)
