c88c4c610bca4b6e7ab9f403125c5706b6d93876
Thore Husfeldt <thore.husfeldt@gmail.com>
2019-09-24 14:40:08 +0200

    Clean up AC submission for intervalscheduling

--- intervalscheduling/submissions/accepted/intervalscheduling.py
+++ intervalscheduling/submissions/accepted/intervalscheduling.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
-I = [tuple(map (int, raw_input().split())) for _ in range(int(raw_input()))]
-I.sort(key = lambda t: t[1])
+from __future__ import print_function 
+from sys import stdin
+n = int(stdin.readline())
+I = []
+for i in range(0, n):
+    s, f = stdin.readline().split()
+    I.append((int(s), int(f)))
+I.sort(key = lambda t: t[-1])
 next_idle = 0
 res = 0
 for start, finish in I:
     if start >= next_idle:
         res += 1
         next_idle = finish
-print res
+print (res)
 
