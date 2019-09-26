42e120426719c0dddcf552d2bca93db223c911d6
Thore Husfeldt <thore.husfeldt@gmail.com>
2019-09-26 22:41:29 +0200

    Add optimized brute-force C solution to TLE dir

---
+++ barshelf/submissions/time_limit_exceeded/fast-brute.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+
+#define M 100000
+
+int data[M];
+
+void readn(int *n) {
+  char c;
+  *n = getc_unlocked(stdin) - '0';
+  while((c = getc_unlocked(stdin)) != ' ') {
+    if (c == '\n') return;
+    else {
+      *n = 10 * (*n) + c - '0';
+    }
+  }
+}
+
+int main() {
+    int n;
+    readn(&n);
+    int ans = 0;
+    for (int i = 0 ; i < n ; ++i) {
+        readn(&data[i]);
+        int tall = data[i] << 1;
+        for (int j = 0 ; j < i ; ++j) {
+            if (data[j] > tall) ++ans;
+        }
+    }
+    printf("%d\n", ans);
+}
