d22f21e55295412a613ee5fa28f3d68ac6663518
Thore Husfeldt <thore.husfeldt@gmail.com>
2019-10-09 23:29:38 +0200

    elderscrollbar: make another AC solution consistent

--- elderscrollbar/submissions/accepted/using_textwrap_library.py
+++ elderscrollbar/submissions/accepted/using_textwrap_library.py
@@ -6,7 +6,7 @@ text = ' '.join([stdin.readline().strip() for _ in range(N)])
 
 typesettext = [ line[:W] for line in wrap(text, W, break_long_words = False) ]
 
-thumbpos = int(L * (H-2) / (len(typesettext) - H + 1)) + 1
+thumbpos = int(L * (H-3) / (len(typesettext) - H )) + 1
 scrollbar = ['^'] + [' '] * (H - 2) + ['v']
 scrollbar[thumbpos] = 'X'
 
