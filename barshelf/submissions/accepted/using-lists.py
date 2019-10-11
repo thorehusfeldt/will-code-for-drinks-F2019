bce39824305e78ba2fe5b1ac6e6b824585d68482
Thore Husfeldt <thore.husfeldt@gmail.com>
2019-10-11 23:17:48 +0200

    barshelf: two AC solution

---
+++ barshelf/submissions/accepted/using-lists.py
@@ -0,0 +1,18 @@
+from sys import stdin
+from bisect import bisect_left, bisect_right, insort
+n = int(stdin.readline())
+H = list(map(int, stdin.readline().split()))
+smaller = [0] * n
+larger  = [0] * n
+left = []
+right = []
+
+for i in range(n):
+    larger[i] = len(left) - bisect_left(left, H[i] * 2)
+    insort(left, H[i])
+
+for i in reversed(range(n)):
+    smaller[i] = bisect_right(right, H[i] / 2)
+    insort(right, H[i])
+
+print ( sum([ larger[i] * smaller[i] for i in range(n)]) )
